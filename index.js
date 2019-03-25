762cf1b576b416e4aa5520a96cb268270a8c137d
Lucas Firmo <lucasmfir@gmail.com>
2019-03-24 23:46:09 -0300

    cors

--- index.js
+++ index.js
@@ -33,11 +33,24 @@ connection.connect(err => {
   }
 })
 
-app.use(cors())
+app.use(function (req, res, next) {
 
-app.get('/', (req, res) => {
-  res.send("ok")
-})
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 app.get('/unis', (req, res) => {
   connection.query(SELECT_ALL_UNIS, (err, results) => {
